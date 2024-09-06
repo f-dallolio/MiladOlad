f3f74f4c326eb4eafcaffb0123a6e23f511f6642
Jane Doe <jane@example.org>
2024-09-06 14:23:43 -0400

    .

--- R/new_script.R
+++ R/new_script.R
@@ -1,5 +1,5 @@
 #' @export
-new_script <- function(name, dir = "scripts", open = rlang::is_interactive()) {
+new_script <- function(name, dir = "scripts", ..., from = NULL, open = rlang::is_interactive()) {
   name <- format(substitute(name))
   dir <- file.path(rprojroot::find_package_root_file(), dir)
   if(!dir.exists(dir)) dir.create(dir)
@@ -9,6 +9,10 @@ new_script <- function(name, dir = "scripts", open = rlang::is_interactive()) {
   out <- c("devtools::document(quiet = TRUE)",
            "devtools::load_all()",
            "", "")
+  if(length(from)){
+    out <- c(out, readLines(from))
+  }
+
   write(out, path)
   rstudioapi::documentOpen(path, line =-1)
   invisible(TRUE)
